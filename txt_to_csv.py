1dc2d2f00e76c7eeb6363e523388c85172d3008e
Peter Wu <peterwu0228@icloud.com>
2020-07-18 20:47:41 +0800

    Update txt_to_csv.py

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,4 +1,9 @@
+## using pandas library
+
 import pandas as pd
 
+# @sep: can be space, tab, comma, etc.
 read_file = pd.read_csv ('textfile.txt', sep=' ')
+
+# @index: whether or not to have an index column
 read_file.to_csv('outputcsv.csv', index=None)
